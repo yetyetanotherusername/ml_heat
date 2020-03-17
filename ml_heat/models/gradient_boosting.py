11c7b157e8473a03ebbe8d7d5083e6448ce804b2
Sebastian Grill <sebastian.grill@smaxtec.com>
2020-03-17 10:33:14 +0100

    WIP model

---
+++ ml_heat/models/gradient_boosting.py
@@ -0,0 +1,21 @@
+import keras as ks
+import vaex as vx
+
+
+class GradientBoostedTrees(object):
+    def __init__(self, organisation_ids=None):
+        self.organisation_ids = organisation_ids
+
+    
+
+    def run(self):
+        pass
+
+
+def main():
+    obj = GradientBoostedTrees()
+    obj.run()
+
+
+if __name__ == '__main__':
+    main()
