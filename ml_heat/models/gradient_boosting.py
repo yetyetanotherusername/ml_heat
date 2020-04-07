a67e58d62da8deb602e4ebd5d0dda4284b051bc1
Sebastian Grill <sebastian.grill@gmx.at>
2020-04-07 19:33:43 +0200

    WIP shifting function

--- ml_heat/models/gradient_boosting.py
+++ ml_heat/models/gradient_boosting.py
@@ -1,11 +1,35 @@
 import xgboost as xg
 import vaex as vx
+import numpy as np
 
 from ml_heat.preprocessing.transform_data import DataTransformer
 from ml_heat.helper import (
     load_data,
     load_vxframe
 )
+
+
+@vx.register_dataframe_accessor('mlheat', override=True)
+class mlheat:
+    def __init__(self, df):
+        self.df = df
+
+    def shift(self, column, n, cyclic=True, fill_value=None, inplace=False):
+        # make a copy without column
+        df = self.df.copy().drop(column)
+        # make a copy with just the colum
+        df_column = self.df[[column]]
+        # slice off the head and tail
+        if cyclic:
+            df_head = df_column[-n:]
+        else:
+            df_head = vx.from_dict(
+                {column: np.ma.masked_all(n, dtype=self.df[column].dtype)})
+        df_tail = df_column[:-n]
+        # stitch them together
+        df_shifted = df_head.concat(df_tail)
+        # and join (based on row number)
+        return df.join(df_shifted, inplace=inplace)
 
 
 class GradientBoostedTrees(object):
@@ -15,7 +39,12 @@ class GradientBoostedTrees(object):
         self.data = load_vxframe(self.vxstore)
 
     def prepare_data(self):
-        pass
+        x = np.arange(10)
+        y = x**2
+        df = vx.from_arrays(x=x, y=y)
+        df['shifted_y'] = df.y
+        df2 = df.mlheat.shift('shifted_y', -2, cyclic=True, fill_value=np.nan)
+        print(df2)
 
     def train_model(self):
         pass
