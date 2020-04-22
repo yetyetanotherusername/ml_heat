11fefa4133cd34c126550e435b3066c3d59930cd
Sebastian Grill <sebastian.grill@gmx.at>
2020-04-22 17:03:21 +0200

    gradient boosting WIP

--- ml_heat/helper.py
+++ ml_heat/helper.py
@@ -20,9 +20,10 @@ def load_organisation(path, organisation_id):
     return frame
 
 
-def duplicate_shift(series, shift, name=None):
-    frame = pd.concat([series] * shift, axis=1)
-    frame.columns = range(shift)
+def duplicate_shift(series, shifts, name=None):
+    n_cols = len(shifts)
+    frame = pd.concat([series] * n_cols, axis=1)
+    frame.columns = shifts
     frame = frame.apply(lambda x: x.shift(int(x.name)))
     if name is not None:
         frame.columns = [f'{name}{column}' for column in frame.columns]
--- ml_heat/models/gradient_boosting.py
+++ ml_heat/models/gradient_boosting.py
@@ -1,6 +1,11 @@
+import os
 import xgboost as xg
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 from ml_heat.preprocessing.transform_data import DataTransformer
 
 from ml_heat.helper import (
@@ -8,26 +13,31 @@ from ml_heat.helper import (
     duplicate_shift
 )
 
-tf = DataTransformer()
+dt = DataTransformer()
 
 
 class GradientBoostedTrees(object):
     def __init__(self, organisation_ids=None):
         self.organisation_ids = organisation_ids
-        self.store = tf.feather_store
+        self.store = dt.feather_store
         if self.organisation_ids is None:
-            organisation_ids = tf.organisation_ids
+            self.organisation_ids = os.listdir(self.store)
+        self.animal = None
+        self.X = pd.DataFrame()
+        self.y = pd.Series()
+        self.X_test = None
+        self.y_test = None
 
-    def prepare_animal(self, organisation_id):
-        data = load_organisation(self.store, organisation_id)
-        data = data.loc[(
-            slice(None),
-            slice(None),
-            '59e75f2b9e182f68cf25721d',
-            slice(None)
-        ), slice(None)]
+        self.model = xg.XGBClassifier(
+            tree_method='gpu_hist',
+            verbose=3,
+            n_estimators=10
+        )
 
-        data = data.droplevel(['organisation_id', 'group_id', 'animal_id'])
+    def prepare_animal(self):
+        data = self.animal
+
+        # data = data.droplevel(['organisation_id', 'group_id', 'animal_id'])
 
         data['annotation'] = np.logical_or(
             np.logical_or(data.pregnant, data.cyclic), data.inseminated)
@@ -43,21 +53,55 @@ class GradientBoostedTrees(object):
             'deleted'
         ], axis=1)
 
-        days = 1
-        shift = days * 144
+        data.act = pd.to_numeric(data.act.round(decimals=2), downcast='float')
+        data.act_group_mean = pd.to_numeric(
+            data.act_group_mean.round(decimals=2), downcast='float')
 
-        act_shift = duplicate_shift(data.act, shift, 'act')
+        shifts = range(144)
+
+        act_shift = duplicate_shift(data.act, shifts, 'act')
         group_shift = duplicate_shift(
-            data.act_group_mean, shift, 'act_group_mean')
+            data.act_group_mean, shifts, 'act_group_mean')
 
         data = pd.concat([data, act_shift, group_shift], axis=1)
         data = data.drop(['act', 'act_group_mean'], axis=1)
         data = data.dropna()
-        print(data.info())
+        data.DIM = pd.to_numeric(data.DIM, downcast='signed')
+        self.y = self.y.append(data.annotation)
+        self.X = self.X.append(data.drop('annotation', axis=1))
+
+    def fit_model(self):
+        self.model.fit(
+            self.X,
+            self.y,
+            verbose=True,
+            # eval_set=[(self.X, self.y), (self.X_test, self.y_test)],
+            # eval_metric='rmse'
+        )
+
+    def loop_organisation(self, organisation_id):
+        data = load_organisation(self.store, organisation_id)
+
+        animal_ids = data.index.unique(level='animal_id')
+
+        for animal_id in tqdm(animal_ids):
+            self.animal = data.loc[(
+                slice(None),
+                slice(None),
+                animal_id,
+                slice(None)
+            ), slice(None)]
+
+            self.prepare_animal()
+
+    def split_data(self):
+        self.X, self.X_test, self.y, self.y_test = train_test_split(
+            self.X, self.y, test_size=0.33, random_state=42)
 
     def run(self):
-        self.prepare_data()
-        # self.train_model()
+        self.loop_organisation('59e7515edb84e482acce8339')
+        # self.split_data()
+        self.fit_model()
         # self.evaluate_model()
         # self.get_data()
         # self.test()
